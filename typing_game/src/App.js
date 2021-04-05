b3df81da96e10264762a2b053c8d98af6851f21a
Palak Gupta <palakgupta2712@gmail.com>
2021-04-05 23:57:24 +0530

    Typing Game - Part 2

--- typing_game/src/App.js
+++ typing_game/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import {Container, Header, TextArea, Button} from 'semantic-ui-react'
 function App() {
+  const [text, setText] = useState("")
+  function handleInput(event){
+    setText(event.target.value)
+  }
+  console.log(text)
+
   return (
     <Container  textAlign='center'>
       <br/>
       <Header as='h1'> Speed Typing Game</Header>
-      <TextArea className="textarea"/>
+      <TextArea 
+          className="textarea"
+          value={text}
+          onChange={handleInput}    
+      />
       <br/>
       <Header as='h3'> Time Remaining : </Header>
       <Button color='blue'>START</Button>
