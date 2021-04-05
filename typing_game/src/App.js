7108f5f1724e3473bd493ca3339acbb03c16c4a1
Palak Gupta <palakgupta2712@gmail.com>
2021-04-05 23:46:23 +0530

    Typing Game - Part 1

--- typing_game/src/App.js
+++ typing_game/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import 'semantic-ui-css/semantic.min.css'
+import {Container, Header, TextArea, Button} from 'semantic-ui-react'
 function App() {
   return (
-    <div>
-      <h1> Speed Typing Game</h1>
-    </div>
+    <Container  textAlign='center'>
+      <br/>
+      <Header as='h1'> Speed Typing Game</Header>
+      <TextArea className="textarea"/>
+      <br/>
+      <Header as='h3'> Time Remaining : </Header>
+      <Button color='blue'>START</Button>
+    </Container>
   );
 }
 
