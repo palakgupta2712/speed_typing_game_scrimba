2552b8e4be419c1bc53f10460916379b38885cf7
Palak Gupta <palakgupta2712@gmail.com>
2021-04-06 00:47:30 +0530

    Typing Game - Part 4:Countdown Timer

--- typing_game/src/App.js
+++ typing_game/src/App.js
@@ -1,8 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import {Container, Header, TextArea, Button} from 'semantic-ui-react'
 function App() {
   const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(5)
+
+  useEffect(() => {
+    if(timeRemaining>0){
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1 )
+        console.log(timeRemaining)
+    }, 1000)
+  }},[timeRemaining])
+
   function handleInput(event){
     setText(event.target.value)
   }
@@ -13,7 +23,7 @@ function App() {
     console.log(filterWords.length)
     return filterWords
   }
-  console.log(text)
+  
 
   return (
     <Container  textAlign='center'>
@@ -25,7 +35,7 @@ function App() {
           onChange={handleInput}    
       />
       <br/>
-      <Header as='h3'> Time Remaining : </Header>
+      <Header as='h3'> Time Remaining : {timeRemaining}</Header>
       <Button color='blue' onClick={() => wordCount(text)}>START</Button>
       <Header as='h3'> Word Count : </Header>
     </Container>
